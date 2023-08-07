758cfd2d8f346831d3b3647e7414bb8e0450570f
Denis Popkov <pkvdenis@gmail.com>
2023-08-07 19:51:23 +0300

    kt4j: add project.

---
+++ kt4j/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") apply false
+}
+
+group = "learn.kt4j"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
+
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ kt4j/settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "kt4j"
+include("m0102t01tracker")
