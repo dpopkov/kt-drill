86d9e988d94a5b1eed9c2b20102f23d81d59c1ce
Denis Popkov <pkvdenis@gmail.com>
2023-09-11 19:25:34 +0300

    bounty-board: functions, numbers, strings.

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -1,7 +1,57 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+const val HERO_NAME: String = "Madrigal"
+var playerLevel: Int = 0
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    println("$HERO_NAME announces her presence to the world.")
+    println("What level is $HERO_NAME?")
+    val playerLevelInput = readln()
+    playerLevel = if (playerLevelInput.matches("""\d+""".toRegex())) {
+        playerLevelInput.toInt()
+    } else {
+        1
+    }
+    println("$HERO_NAME's level is $playerLevel")
+
+    readBountyBoard()
+
+    println("Time passes...")
+    println("$HERO_NAME returns from her quest.")
+
+    playerLevel += 1
+    println(playerLevel)
+    readBountyBoard()
+}
+
+private fun readBountyBoard() {
+    println(
+        """
+        $HERO_NAME approaches the bounty board. It reads:
+        	"${obtainQuest(playerLevel).replace("[Nn]ogartse".toRegex(), "xxxxxxxx")}"
+        """.trimIndent()
+    )
+}
+
+private fun obtainQuest(
+    playerLevel: Int,
+    playerClass: String = "paladin",
+    hasBefriendedBarbarians: Boolean = true,
+    hasAngeredBarbarians: Boolean = false
+): String = when (playerLevel) {
+    1 -> "Meet Mr.Bubble in the land of soft things."
+
+    in 2..5 -> {
+        // Проверить возможность дипломатического решения
+        val canTalkToBarbarians =
+            !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "barbarian")
+        if (canTalkToBarbarians) {
+            "Convince the barbarians to call off their invasion."
+        } else {
+            "Save the town from the barbarian invasions."
+        }
+    }
+
+    6 -> "Locate the enchanted sword."
+    7 -> "Recover the long-lost artifact of creation."
+    8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+    else -> "There are no quests right now."
 }
