0e188274cdd8681fd6c0198eae5f915c35a5783c
Denis Popkov <pkvdenis@gmail.com>
2023-09-24 21:16:31 +0300

    zh-roadmap: p3Currencies: add project.

---
+++ zh-roadmap/p3Currencies/src/main/kotlin/CurrenciesApplication.kt
@@ -0,0 +1,11 @@
+package learn.zhroadmap.p3currencies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurrenciesApplication
+
+fun main(args: Array<String>) {
+	runApplication<CurrenciesApplication>(*args)
+}
---
+++ zh-roadmap/p3Currencies/src/test/kotlin/CurrenciesApplicationTests.kt
@@ -0,0 +1,13 @@
+package learn.zhroadmap.p3currencies
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrenciesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
